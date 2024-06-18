62884347c3136881b4529a3dc2e323675095c967
arun <arunkvijay@gmail.com>
2024-06-18 11:18:38 +0530

    changed 18th june

--- ifc/codes/python_codes/grid_axis.py
+++
@@ -1,7 +0,0 @@
-def uaxis(grid, start_char, end_char):
-    for char_code in range(ord(start_char), ord(end_char) + 1):
-        ifcopenshell.api.run("grid.create_grid_axis", f, axis_tag=chr(char_code), uvw_axes="UAxes", grid=grid)
-
-def vaxis(grid, start_num, end_num):
-    for i in range(start_num, end_num + 1):
-        ifcopenshell.api.run("grid.create_grid_axis", f, axis_tag=str(i), uvw_axes="VAxes", grid=grid)
--- ifc/codes/python_codes/replace_attribute.py
+++
@@ -1,3 +0,0 @@
-for ele in f.traverse(wall):
-    if hasattr(ele, 'Depth'):
-        ifcopenshell.util.element.replace_attribute(ele, 1500, 600)
